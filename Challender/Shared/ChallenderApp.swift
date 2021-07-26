04f65f1434ec88a654c8ebb0e8011211241ad93d
Kim, GiBeom <gibum1228@naver.com>
2021-07-26 14:30:45 +0900

    init

---
+++ Challender/Shared/ChallenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallenderApp.swift
+//  Shared
+//
+//  Created by gibeom on 2021/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ChallenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
