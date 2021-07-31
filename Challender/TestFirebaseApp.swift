72cada5ad9d9485c834c58be636cf767f98f59ef
Kim, GiBeom <gibum1228@naver.com>
2021-08-01 00:29:59 +0900

    gitignore update

---
+++ Challender/TestFirebaseApp.swift
@@ -0,0 +1,21 @@
+//
+//  FirebaseApp.swift
+//  Challender
+//
+//  Created by gibeom on 2021/07/31.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct FirebaseApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        
+    }
+}
